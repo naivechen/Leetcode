03a442fef0e77bea048cb82f2aa1f666339686ba
Mingming Chen <naivechen02@gmail.com>
2014-11-15 09:12:02 +0800

    bit manipulation

---
+++ Single-Number.cpp
@@ -0,0 +1,12 @@
+// https://oj.leetcode.com/problems/single-number/
+
+
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int ans=0;
+        for(int i=0;i<n;i++)
+            ans=ans^A[i];
+        return ans;
+    }
+};
