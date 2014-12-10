3cd4045403df65e3b2fff116589b4f1967af4b1e
Mingming Chen <naivechen02@gmail.com>
2014-12-10 09:01:17 +0800

    easy

---
+++ Search-a-2D-Matrix.cpp
@@ -0,0 +1,18 @@
+// https://oj.leetcode.com/problems/search-a-2d-matrix/
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int i=n-1,j=0;
+        while(i>=0 && j<m)
+        {
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]<target) j++;
+            else i--;
+        }
+        return false;
+    }
+};
+
