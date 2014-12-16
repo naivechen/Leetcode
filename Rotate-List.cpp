f92ab4086056a3d389b200fabb3bd23b744ecad7
Mingming Chen <naivechen02@gmail.com>
2014-12-16 15:55:35 +0800

    list

---
+++ Rotate-List.cpp
@@ -0,0 +1,33 @@
+// https://oj.leetcode.com/problems/rotate-list/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *rotateRight(ListNode *head, int k) {
+        if(head==NULL || k==0) return head;
+        int len=0;
+        ListNode *ptr=head, *tail=head;
+        while(ptr!=NULL)
+        {
+            len++;
+            tail=ptr;
+            ptr=ptr->next;
+        }
+        k%=len;
+        ptr=head;
+        for(int i=0;i<len-k-1;i++)
+            ptr=ptr->next;
+        tail->next=head;
+        head=ptr->next;
+        ptr->next=NULL;
+        return head;
+    }
+};
