14411546f44fb4058f0cb8796ce99bc47c039368
Mingming Chen <naivechen02@gmail.com>
2015-01-06 14:35:05 +0800

    string

---
+++ Excel-Sheet-Column-Title.cpp
@@ -0,0 +1,23 @@
+// https://oj.leetcode.com/problems/excel-sheet-column-title/
+
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        if(n < 1)
+            return "";
+        else
+        {
+            string result = "";
+            while(n)
+            {
+                n --;
+                char c = n%26 + 'A';
+                result += c;
+                n /= 26;
+            }
+            reverse(result.begin(), result.end());
+            return result;
+        }
+    }
+};
