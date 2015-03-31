bd704fa196fa23d62cc803158ac853b57835f4b1
Mingming Chen <naivechen02@gmail.com>
2015-03-31 20:22:30 +0800

    easy

---
+++ Number of 1 Bits.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/number-of-1-bits/
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while(n!=0)
+        {
+            if(n & 1) ans++;
+            n/=2;
+        }
+        return ans;
+    }
+};
