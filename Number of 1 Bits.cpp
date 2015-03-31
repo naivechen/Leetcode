68b8a3f9ca27a632f71c312c54064b6a346e0be5
Mingming Chen <naivechen02@gmail.com>
2015-03-31 20:52:12 +0800

    easy

--- Number of 1 Bits.cpp
+++ Number of 1 Bits.cpp
@@ -13,3 +13,18 @@ public:
         return ans;
     }
 };
+
+
+// more fast code
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while(n!=0)
+        {
+            ans++;
+            n=n&(n-1);
+        }
+        return ans;
+    }
+};
